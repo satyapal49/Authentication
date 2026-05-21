8d3b16330e42132264321f8b0ce0f991969cf43c
Satyapal Raj <satyapalraj@gmail.com>
2026-05-21 23:53:56 +0530

    feat: add StreamTransport for streaming email messages
    - Implemented StreamTransport class for handling email message streaming.
    - Added logging for message sending with support for buffer and newline options.
    
    feat: introduce well-known services for SMTP configuration
    
    - Created well-known services module to resolve SMTP configurations based on service names, aliases, or domains.
    - Added normalization functions for service keys and services.
    
    feat: populate services.json with various email service configurations
    
    - Added comprehensive list of email service providers with their SMTP settings in services.json.
    
    feat: implement XOAuth2 token generation for Gmail
    
    - Developed XOAuth2 class for generating access tokens for Gmail using OAuth2.
    - Included methods for token renewal, updating tokens, and generating JWTs.
    
    chore: update nodemailer package.json with necessary metadata
    
    - Added package.json for nodemailer with relevant metadata, scripts, and dependencies.

--- Backend/config/zod.js
+++ Backend/config/zod.js
@@ -1,7 +1,7 @@
 import {email, z} from 'zod'
 
 export const registerSchema = z.object({
-    name: z.string().min(2, "Name must be at least 3 character long"),
+    name: z.string().min(3, "Name must be at least 3 character long"),
     email: z.string().email("Invalid email format"),
     password: z.string().min(8, "password must be atleast 8 characters long")
 });
--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,3 +1,4 @@
+import { ZodError } from 'zod';
 import { registerSchema } from '../config/zod.js';
 import TryCatch from '../middlewares/tryCatch.js';
 import sanitize from 'mongo-sanitize';
@@ -7,9 +8,25 @@ export const registerUser = TryCatch(async(req, res)=>{
     const validation = registerSchema.safeParse(sanitizeBody);
 
     if(!validation.success){
+        let zoderror = validation.error;
+        let firstErrorMessage = "Validation error";
+        let allError = [];
+
+        if(zoderror?.issues && Array.isArray(zoderror.issues)){
+            allError = zoderror.issues.map((issue)=>({
+                field: issue.path ? issue.path.join(".") : "unknown",
+                    message: issue.message || "Validation Error",
+                    code: issue.code, 
+            }));
+
+            firstErrorMessage = allError[0]?.message || "Validation Error";
+            
+        }
+
         return res.status(400).json({
-            message: "Validation error",
-        })
+            message: firstErrorMessage,
+            error: allError,
+        });
     }
 
     const {name, email, password} = validation.data;
